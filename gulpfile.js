ab4365f8b6e22de8d7eb11b3b12a59c05fa7a3ae
Beyaty Dávid <beyaty.david@brightly.hu>
2018-06-12 16:10:22 +0200

    things

--- gulpfile.js
+++ gulpfile.js
@@ -41,19 +41,19 @@ var onJSError = function (err) {
 gulp.task('styles:sass', function () {
 
     return gulp.src(path.join(config.sass.path.src, '**', '*.scss'))
-        .pipe(sourcemaps.init())
+        //.pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(sourcemaps.write("./"))
+        //.pipe(sourcemaps.write("./"))
         .pipe(gulp.dest(config.sass.path.dist));
 });
 
 gulp.task('javascript', function () {
     return gulp.src(['./node_modules/js-cookie/src/js.cookie.js', 'src/js/templates.js','src/js/script.js', 'src/langs/en.js'])
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(concat('script.js'))
     .pipe(uglify())
-    .pipe(sourcemaps.write("./"))
+    //.pipe(sourcemaps.write("./"))
     .pipe(gulp.dest(config.javascript.path.dist))
 });
 
