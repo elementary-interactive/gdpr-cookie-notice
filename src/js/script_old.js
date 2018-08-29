9d966b3c05f8d82ede6745d8b811129673663049
Beyaty David <beyaty.david@brightly.hu>
2018-08-29 15:25:55 +0200

    .

--- src/js/script_old.js
+++ src/js/script_old.js
@@ -81,10 +81,7 @@ function gdprCookieNotice(config) {
 
   // Hide cookie notice bar
   function hideNotice() {
-    if(document.documentElement.classList.parentNode) {
-      document.documentElement.classList.parentNode.removeChild(pluginPrefix+'-loaded');
-    }
-  //  document.documentElement.classList.remove(pluginPrefix+'-loaded');
+    document.documentElement.classList.remove(pluginPrefix+'-loaded');
   }
 
   // Write gdpr cookie notice's cookies when user accepts cookies
@@ -192,13 +189,8 @@ function gdprCookieNotice(config) {
     var label = document.querySelector('.'+pluginPrefix+'-modal-cookie-input-switch');
     label.innerHTML = gdprCookieNoticeLocales[config.locale]['always_on'];
     label.classList.add(pluginPrefix+'-modal-cookie-state');
-    if(label.classList.parentNode) {
-      label.classList.parentNode.removeChild(pluginPrefix+'-modal-cookie-input-switch');
-    }
-    if(input.parentNode) {
-      input.parentNode.removeChild(input);
-    }
-
+    //label.classList.remove(pluginPrefix+'-modal-cookie-input-switch');
+    input.remove();
 
     // Load other categories if needed
     if(config.performance) categoryList.innerHTML += localizeTemplate('category.html', 'cookie_performance');
@@ -231,10 +223,7 @@ function gdprCookieNotice(config) {
 
   // Hide modal window
   function hideModal() {
-    if(document.documentElement.classList.parentNode) {
-      document.documentElement.classList.parentNode.removeChild(pluginPrefix+'-show-modal');
-    }
-    //document.documentElement.classList.remove(pluginPrefix+'-show-modal');
+    document.documentElement.classList.remove(pluginPrefix+'-show-modal');
   }
 
   // Click functions in the notice
@@ -286,7 +275,7 @@ function gdprCookieNotice(config) {
       e.preventDefault();
       saveButton.classList.add('saved');
       setTimeout(function(){
-        saveButton.classList.parentNode.removeChild('saved');
+        saveButton.classList.remove('saved');
       }, 1000);
       acceptCookies(true);
       setTimeout(function(){
