babbc7db0bef3b9fe16436cf0fd80bf2d38a2497
Beyaty David <beyaty.david@brightly.hu>
2018-08-29 15:59:06 +0200

    things

--- dist/script.js
+++ dist/script.js
@@ -225,6 +225,23 @@ try {
 	return init(function () {});
 }));
 
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 //HEAD 
 window["gdpr-cookie-notice-templates"] = {};
 
